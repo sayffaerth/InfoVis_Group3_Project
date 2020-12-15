134ace6de6aa8e9c3409ea0e5f1bdb303487f9c6
alxschmidt <45689749+alxschmidt@users.noreply.github.com>
2020-12-15 01:31:10 +0100

    A few more fixes with integration and code organization
    Unfortunately couldn't achieve covid case to color mapping so far

--- grafik.js
+++ grafik.js
@@ -1,14 +1,54 @@
-//The svg
+//Load the SVG element that shall contain the visualisation
 var svg = d3.select("#my_dataviz"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-// Map and projection
+// %%%%%%%%%%%%%%%%%%%%
+// % Map & projection %
+// %%%%%%%%%%%%%%%%%%%%
+
 var path = d3.geoPath();
 var projection = d3.geoMercator()
     .scale(2000)        // This is like the zoom
     .center([15,51.2])  // GPS of location to zoom on
     .translate([width / 2, height / 2]);
+
+// Data and color scale
+var data = d3.map();
+var colorScale = d3.scaleThreshold()
+    .domain([0, 1, 2, 3, 4, 5])
+    .range(d3.schemeBlues[7]);
+
+// Fetch JSON Data and use it in visualisation as soon as it's done loading
+d3.queue()
+    .defer(d3.json, "https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.geojson")
+    .await(dataLoaded);
+
+// Call this as soon as the data is loaded to use it in the visualisation
+function dataLoaded(error, topo) {
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d) {
+            d.total = data.get(d.id) || 0;
+            return colorScale(d.total);
+        })
+        .style("stroke", "#fff")
+        .on("mouseover", mouseOver )
+        .on("mousemove", mouseMove)
+        .on("mouseleave", mouseLeave );
+}
+// %%%%%%%%%%%%%%%%%%%%
+// % Tooltip          %
+// %%%%%%%%%%%%%%%%%%%%
 
 
 var tooltip = d3.select("#my_dataviz").append("g")
@@ -29,58 +69,9 @@ tooltip.append("g:text")
     .attr("font-size", "12px")
     .attr("font-weight", "bold");
 
-/*
-// Load external data and boot
-d3.json("./Data/bundeslaender_simplify200.geojson", function (data) {
-
-// Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .enter().append("path")
-        .attr("fill", "#b50000")
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        .style("stroke", "#fff")
-        .on("mouseover", mouseOver)
-        .on("mousemove", mouseMove)
-        .on("mouseleave", mouseLeave)
-});
-*/
-
-// Data and color scale
-var data = d3.map();
-var colorScale = d3.scaleThreshold()
-    .domain([0, 10000, 20000, 30000, 40000, 50000])
-    .range(d3.schemeBlues[7]);
-
-// Load external data and boot
-d3.queue()
-    .defer(d3.json, "https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.geojson")
-    .await(ready);
-
-function ready(error, topo) {
-
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(topo.features)
-        .enter()
-        .append("path")
-        // draw each country
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        // set the color of each country
-        .attr("fill", function (d) {
-            d.total = data.get(d.id) || 0;
-            return colorScale(d.total);
-        })
-        .style("stroke", "#fff")
-        .on("mouseover", mouseOver )
-        .on("mouseleave", mouseLeave );
-}
+// %%%%%%%%%%%%%%%%%%%%
+// % Helper functions %
+// %%%%%%%%%%%%%%%%%%%%
 
 let mouseOver = function (d) {
     tooltip
@@ -96,6 +87,7 @@ let mouseOver = function (d) {
         .duration(200)
         .style("opacity", 0.8)
         .style("stroke", "steelblue")
+        .style("stroke-opacity", 1)
 };
 
 let mouseMove = function (d) {
@@ -120,5 +112,7 @@ let mouseLeave = function (d) {
         .transition()
         .duration(200)
         .style("opacity", 1)
-        .style("stroke", "transparent")
+        .style("stroke", "white")
+        .style("stroke-opacity", 0.2)
+
 };
