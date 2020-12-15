f54420f160e761e3a976b63a79c7abfc108d8810
ru89vis <clara.sayffaerth@campus.lmu.de>
2020-12-15 20:07:26 +0100

    Graph mit Hover with Line

--- script.js
+++ script.js
@@ -13,129 +13,144 @@ function salesData(data){
 
 // Visualisiere Fallzahlen Funktion
 function visualizeAsLineChart(data) {
-  var margin = { top: 30, right: 120, bottom: 30, left: 50 },
-  width = 800 - margin.left - margin.right,
-  height = 200 - margin.top - margin.bottom,
-  tooltip = { width: 100, height: 100, x: 10, y: -30 };
+  var margin = { top: 10, right: 120, bottom: 20, left: 50 },
+        width = 700 - margin.left - margin.right,
+        height = 170 - margin.top - margin.bottom,
+        tooltip = { width: 100, height: 100, x: 10, y: -30 };
 
-var parseDate = d3.timeParse("%d/%m/%Y"),
-  bisectDate = d3.bisector(function(d) { return d.dateRep; }).left,
-  formatValue = d3.format(","),
-  dateFormatter = d3.timeFormat("%d/%m/%y");
+    var parseDate = d3.timeParse("%d/%m/%Y"),
+        bisectDate = d3.bisector(function(d) { return d.dateRep; }).left,
+        formatValue = d3.format(","),
+        dateFormatter = d3.timeFormat("%d/%m/%y");
 
-var x = d3.scaleTime()
-      .range([0, width]);
+    var x = d3.scaleTime()
+            .range([0, width]);
 
-var y = d3.scaleLinear()
-      .range([height, 0]);
+    var y = d3.scaleLinear()
+            .range([height, 0]);
 
-var xAxis = d3.axisBottom(x)
-  .tickFormat(dateFormatter);
+    var xAxis = d3.axisBottom(x)
+        .tickFormat(dateFormatter);
 
-var yAxis = d3.axisLeft(y)
-  .tickFormat(d3.format("s"))
+    var yAxis = d3.axisLeft(y)
+        .tickFormat(d3.format("s"))
 
-var line = d3.line()
-  .x(function(d) { return x(d.dateRep); })
-  .y(function(d) { return y(d.cases); });
+    var line = d3.line()
+        .x(function(d) { return x(d.dateRep); })
+        .y(function(d) { return y(d.cases); });
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  data.forEach(function(d) {
-      d.dateRep = parseDate(d.dateRep);
-      d.cases = +d.cases;
-  });
+        data.forEach(function(d) {
+            d.dateRep = parseDate(d.dateRep);
+            d.cases = +d.cases;
+        });
 
-  data.sort(function(a, b) {
-      return a.dateRep - b.dateRep;
-  });
+        data.sort(function(a, b) {
+            return a.dateRep - b.dateRep;
+        });
 
-  x.domain([data[0].dateRep, data[data.length - 1].dateRep]);
-  y.domain(d3.extent(data, function(d) { return d.cases; }));
+        x.domain([data[0].dateRep, data[data.length - 1].dateRep]);
+        y.domain(d3.extent(data, function(d) { return d.cases; }));
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Fallzahlen");
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Fallzahlen");
 
-  svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("class", "line")
-      .attr("d", line);
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("class", "line")
+            .attr("d", line);
 
-  var focus = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
 
-  focus.append("circle")
-      .attr("r", 5);
+        focus.append("line")
+          .attr("class", "x")
+          .style("stroke", "black")
+          .style("opacity", 0.5)
+          .style("stroke-width", "2px")
+          .attr("y1", -250)
+          .attr("y2", 250);
 
-  focus.append("rect")
-      .attr("class", "tooltip")
-      .attr("width", 100)
-      .attr("height", 50)
-      .attr("x", 10)
-      .attr("y", -22)
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .style("fill", "none")
-      .style("pointer-events", "all");
+        focus.append("circle")
+          .attr("class", "y")
+          .style("fill", "none")
+          .style("stroke", "black")
+          .attr("r", 4);
 
-  focus.append("text")
-      .attr("class", "tooltip-date")
-      .attr("font-family", "calibri")
-      .attr("x", 18)
-      .attr("y", -2);
+        focus.append("rect")
+            .attr("class", "tooltip")
+            .attr("width", 100)
+            .attr("height", 50)
+            .attr("x", 10)
+            .attr("y", -22)
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .style("fill", "white")
+            .style("opacity", 0.7)
+            .style("pointer-events", "all");
 
-  focus.append("text")
-      .attr("x", 18)
-      .attr("y", 18)
-      .attr("font-family", "calibri")
-      .text("Fallzahl:");
+        focus.append("text")
+            .attr("class", "tooltip-date")
+            .attr("font-family", "calibri")
+            .attr("font-size","14px")
+            .attr("x", 18)
+            .attr("y", 0);
 
-  focus.append("text")
-      .attr("class", "tooltip-likes")
-      .attr("font-family", "calibri")
-      .attr("x", 75)
-      .attr("y", 18);
+        focus.append("text")
+            .attr("x", 18)
+            .attr("y", 18)
+            .attr("font-family", "calibri")
+            .attr("font-size","14px")
+            .text("Fallzahl:");
 
-  svg.append("rect")
-      .attr("class", "overlay")
-      .attr("width", width)
-      .attr("height", height)
-      .style("fill", "none")
-      .style("pointer-events", "all")
-      .on("mouseover", function() { focus.style("display", null); })
-      .on("mouseout", function() { focus.style("display", "none"); })
-      .on("mousemove", mousemove);
+        focus.append("text")
+            .attr("class", "tooltip-likes")
+            .attr("font-family", "calibri")
+            .attr("font-size","14px")
+            .attr("x", 70)
+            .attr("y", 18);
 
-  function mousemove() {
-      var x0 = x.invert(d3.mouse(this)[0]),
-          i = bisectDate(data, x0, 1),
-          d0 = data[i - 1],
-          d1 = data[i],
-          d = x0 - d0.dateRep > d1.dateRep - x0 ? d1 : d0;
-      focus.attr("transform", "translate(" + x(d.dateRep) + "," + y(d.cases) + ")");
-      focus.select(".tooltip-date").text(dateFormatter(d.dateRep));
-      focus.select(".tooltip-likes").text(formatValue(d.cases));
-  }
-}
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.dateRep > d1.dateRep - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x(d.dateRep) + "," + y(d.cases) + ")");
+            focus.select(".tooltip-date").text(dateFormatter(d.dateRep));
+            focus.select(".tooltip-likes").text(formatValue(d.cases));
+        }
+    }
  
 
 //Visualisiere das Race
