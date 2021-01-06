b7ae4300e6f652dd9c980181d4ca49930f0bd8a4
Jojo <johanna.prinz@gmx.de>
2021-01-06 12:49:07 +0100

    Race update mit ohne Flackern

--- script.js
+++ script.js
@@ -115,7 +115,6 @@ function visualizeAsLineChart(data){
         .call(d3.drag()
             .on("start.interrupt", function() { slider.interrupt(); })
             .on("start drag", function() {
-              console.log("DRAG SLIDER");
               currentValue = d3.event.x;
               update(x.invert(currentValue));
               week = update(x.invert(currentValue), "week");
@@ -197,15 +196,13 @@ function visualizeAsLineChart(data){
     
     //Play Funktion
     function step() {
-        console.log("PLAYFUNKTION");
       update(x.invert(currentValue), "week");
       week = update(x.invert(currentValue), "week");
-      console.log(week);
       if(checkContainer("raceContainer")) {
-        replaceContainer("visRace", "visRace");
-        visualizeRace(week); 
+        //replaceContainer("visRace", "visRace");
+        visualizeRace(true, week); 
       }
-      currentValue = currentValue + (targetValue/12000);
+      currentValue = currentValue + (targetValue/2000);
       if (currentValue > targetValue) {
         moving = false;
         currentValue = 0;
@@ -297,11 +294,11 @@ function visualizeAsLineChart(data){
 visualizeRace(false, 0);
 
 function visualizeRace(update, week) {
-    console.log(update);
 d3.json("/Data/absatzindex.json", function(data) {
-    console.log("VisRace");
+        
     if(!update) {
-        console.log(update);
+        console.log(" nicht update");
+
         var margin = {top: 10, right: 30, bottom: 30, left: 60},
         width = 500 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
@@ -386,115 +383,198 @@ d3.json("/Data/absatzindex.json", function(data) {
         // timeChoose(data[i]);}}
         timeChoose(data[week]);
     } else {
-        console.log(update);
-        updateBars(data[week])
+        console.log("update");
+
+        container = d3.select("#raceContainer")
+            .remove();
+        
+            var margin = {top: 10, right: 30, bottom: 30, left: 60},
+            width = 500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+    
+            var svg = d3.select("#visRace")
+                .append("svg")
+                    .attr("id", "raceContainer")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+            //Formatierungen für die Zeit
+            var parseDate = d3.timeParse("%Y/%W"),
+                //bisectDate = d3.bisector(function(d) { return d.Kalenderwoche; }).left,
+                formatValue = d3.format(","),
+                dateFormatterRace = d3.timeFormat("%m");
+    
+            //Formatiert Kalenderwochen in den Daten
+            data.forEach(function(d) {
+                d.Kalenderwoche = parseDate(d.Kalenderwoche);
+            });
+    
+            //Größe Achsen
+            var x = d3.scaleBand()
+                    .range([0, width]).padding(0.7);
+    
+            var y = d3.scaleLinear()
+                    .range([height, 0]);
+    
+            //Position Achse
+            var xAxis = d3.axisBottom(x);
+            var yAxis = d3.axisLeft(y).tickFormat(d3.format(""));
+    
+            //Sortieren
+            data.sort(function(a, b) {
+                return a.Kalenderwoche - b.Kalenderwoche;
+            });
+    
+            //Achsenbeschriftung
+            x.domain(Object.keys(dataToDraw(data)));
+            y.domain([0, 1000]);
+    
+            //X-Achse
+            svg.append("g")
+                .attr("class", "xaxis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+    
+            //Y-Achse
+            svg.append("g")
+                .attr("class", "yaxis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Absatzindex");
+    
+            svg.append("line")
+                .attr("class", "x")
+                .style("stroke", "#ef885a")
+                .style("opacity", 1)
+                .style("stroke-width", "2px")
+                .attr("y1", 414.5)
+                .attr("y2", 414.5)
+                .attr("x1", 0)
+                .attr("x2", 410);
+    
+            svg.append("text")  
+                .attr("class", "hunder")
+                .text("Vorjahres Durchschnitt")
+                .attr("transform", "translate("+ (318) + "," + (410) + ")")
+                .attr("font-family", "calibri")
+                .attr("font-size","10px")
+                .style("fill", "#ef885a")
+                .style("opacity", 1);
+
+        timeChoose(data[week])
     }
 
     function timeChoose(data){
-        console.log("timeChoose");
-    var newData = [
-        {"Product": "Seife", "Count": parseFloat(data.Seife)},
-        {"Product": "Toilettenpapier", Count: parseFloat(data.Toilettenpapier)},
-        {"Product": "Mehl", "Count": parseFloat(data.Mehl)},
-        {"Product": "Desinfektionsmittel", Count: parseFloat(data.Desinfektionsmittel)},
-        {"Product": "Hefe", "Count": parseFloat(data.Hefe)}
-    ]
+
+        var newData = [
+            {"Product": "Seife", "Count": parseFloat(data.Seife)},
+            {"Product": "Toilettenpapier", "Count": parseFloat(data.Toilettenpapier)},
+            {"Product": "Mehl", "Count": parseFloat(data.Mehl)},
+            {"Product": "Desinfektionsmittel", "Count": parseFloat(data.Desinfektionsmittel)},
+            {"Product": "Hefe", "Count": parseFloat(data.Hefe)}
+        ]
     
-    bars = svg.selectAll(".bar")
-        .data(newData)
+        bars = svg.selectAll(".bar")
+            .data(newData)
+
+        bars.enter()
+            .append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(d.Product); })
+                .attr("y", function(d) { return y(d.Count); })
+                .attr("height", function(d) { return height - y(d.Count); })
+                .attr("width", x.bandwidth())
+                .attr("fill", "#9de0ff");
+                //.transition()
+                //.duration(100)
+                //.delay(function(d,i){ return i*50})
+                //.attr("y", function(d) { return y(d.Count); })
+                //.attr("height", function(d) { return height - y(d.Count); })
         
-    bars.enter()
-        .append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(d.Product); })
-            .attr("y", function(d) { return y(d.Count); })
-            .attr("height", function(d) { return height - y(d.Count); })
-            .attr("width", x.bandwidth())
-            .attr("fill", "#9de0ff");
-            //.transition()
-            //.duration(100)
-            //.delay(function(d,i){ return i*50})
-            //.attr("y", function(d) { return y(d.Count); })
-            //.attr("height", function(d) { return height - y(d.Count); })
-    
-    //Bilder
-    var imgSeife = svg.append('image')
-    .attr('href', './Pictures/Verkaufszahlen/Seife_oB.png')
-    .attr('width', 50)
-    .attr('height', 50)
-    .attr("x", 36)
-    .attr("y",  y(110 + newData[0].Count) );
+        //Bilder
+        var imgSeife = svg.append('image')
+        .attr('href', './Pictures/Verkaufszahlen/Seife_oB.png')
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr("x", 36)
+        .attr("y",  y(110 + newData[0].Count) );
 
-    var imgToi = svg.append('image')
-    .attr('href', './Pictures/Verkaufszahlen/Klopapier.png')
-    .attr('width', 50)
-    .attr('height', 50)
-    .attr("x", 108)
-    .attr("y", y(105 + newData[1].Count) );
+        var imgToi = svg.append('image')
+        .attr('href', './Pictures/Verkaufszahlen/Klopapier.png')
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr("x", 108)
+        .attr("y", y(105 + newData[1].Count) );
 
-    var imgMehl = svg.append('image')
-    .attr('href', './Pictures/Verkaufszahlen/Mehl.png')
-    .attr('width', 50)
-    .attr('height', 50)
-    .attr("x", 180)
-    .attr("y", y(100 + newData[2].Count) );
-    
-    var imgDes = svg.append('image')
-    .attr('href', './Pictures/Verkaufszahlen/Desinfektionsmittel.png')
-    .attr('width', 50)
-    .attr('height', 50)
-    .attr("x", 252)
-    .attr("y", y(105 + newData[3].Count) );
-    
-    var imgHefe = svg.append('image')
-    .attr('href', './Pictures/Verkaufszahlen/Hefe.png')
-    .attr('width', 50)
-    .attr('height', 50)
-    .attr("x", 324)
-    .attr("y",  y(90 + newData[4].Count) );
+        var imgMehl = svg.append('image')
+        .attr('href', './Pictures/Verkaufszahlen/Mehl.png')
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr("x", 180)
+        .attr("y", y(100 + newData[2].Count) );
+        
+        var imgDes = svg.append('image')
+        .attr('href', './Pictures/Verkaufszahlen/Desinfektionsmittel.png')
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr("x", 252)
+        .attr("y", y(105 + newData[3].Count) );
+        
+        var imgHefe = svg.append('image')
+        .attr('href', './Pictures/Verkaufszahlen/Hefe.png')
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr("x", 324)
+        .attr("y",  y(90 + newData[4].Count) );
     
 
-    //Zahlen über Balken
-    var textSeife = svg.append("text")  
-        .attr("class", "hunder")
-        .text(newData[0].Count.toString()) 
-        .attr("transform", "translate("+ (61-newData[0].Count.toString().length*3) + "," + y(70 + newData[0].Count) + ")")
-        .attr("font-family", "arial")
-        .attr("font-size","11px")
-        .style("fill", "white")
-        .style("text-align", "center");
+        //Zahlen über Balken
+        var textSeife = svg.append("text")  
+            .attr("class", "hunder")
+            .text(newData[0].Count.toString()) 
+            .attr("transform", "translate("+ (61-newData[0].Count.toString().length*3) + "," + y(70 + newData[0].Count) + ")")
+            .attr("font-family", "arial")
+            .attr("font-size","11px")
+            .style("fill", "white")
+            .style("text-align", "center");
 
-    var textToi = svg.append("text")  
-        .attr("class", "hunder")
-        .text(newData[1].Count.toString())
-        .attr("transform", "translate("+ (132-newData[1].Count.toString().length*3) + "," + y(95 + newData[1].Count) + ")")
-        .attr("font-family", "arial")
-        .attr("font-size","11px")
-        .style("fill", "white");
+        var textToi = svg.append("text")  
+            .attr("class", "hunder")
+            .text(newData[1].Count.toString())
+            .attr("transform", "translate("+ (132-newData[1].Count.toString().length*3) + "," + y(95 + newData[1].Count) + ")")
+            .attr("font-family", "arial")
+            .attr("font-size","11px")
+            .style("fill", "white");
 
-    var textMehl = svg.append("text")  
-        .attr("class", "hunder")
-        .text(newData[2].Count.toString())
-        .attr("transform", "translate("+ (205-newData[2].Count.toString().length*3) + "," + y(95 + newData[2].Count) + ")")
-        .attr("font-family", "arial")
-        .attr("font-size","11px")
-        .style("fill", "white");
+        var textMehl = svg.append("text")  
+            .attr("class", "hunder")
+            .text(newData[2].Count.toString())
+            .attr("transform", "translate("+ (205-newData[2].Count.toString().length*3) + "," + y(95 + newData[2].Count) + ")")
+            .attr("font-family", "arial")
+            .attr("font-size","11px")
+            .style("fill", "white");
 
-    var textDes = svg.append("text")  
-        .attr("class", "hunder")
-        .text(newData[3].Count.toString())
-        .attr("transform", "translate("+ (277-newData[3].Count.toString().length*3) + "," + y(103 + newData[3].Count) + ")")
-        .attr("font-family", "arial")
-        .attr("font-size","11px")
-        .style("fill", "white");
+        var textDes = svg.append("text")  
+            .attr("class", "hunder")
+            .text(newData[3].Count.toString())
+            .attr("transform", "translate("+ (277-newData[3].Count.toString().length*3) + "," + y(103 + newData[3].Count) + ")")
+            .attr("font-family", "arial")
+            .attr("font-size","11px")
+            .style("fill", "white");
 
-    var textHefe = svg.append("text")  
-        .attr("class", "hunder")
-        .text(newData[4].Count.toString())
-        .attr("transform", "translate("+ (348-newData[4].Count.toString().length*3) + "," + y(68 + newData[4].Count) + ")")
-        .attr("font-family", "arial")
-        .attr("font-size","11px")
-        .style("fill", "white");
+        var textHefe = svg.append("text")  
+            .attr("class", "hunder")
+            .text(newData[4].Count.toString())
+            .attr("transform", "translate("+ (348-newData[4].Count.toString().length*3) + "," + y(68 + newData[4].Count) + ")")
+            .attr("font-family", "arial")
+            .attr("font-size","11px")
+            .style("fill", "white");
     }
 
     //Entfernt die Kalenderwochen von den Daten
@@ -508,127 +588,6 @@ d3.json("/Data/absatzindex.json", function(data) {
         }
         return returnData
     }
-
-    function updateBars(data) {
-        console.log("update");
-
-        var svg = d3.select("#raceContainer");
-
-        var newData = [
-            {"Product": "Seife", "Count": parseFloat(data.Seife)},
-            {"Product": "Toilettenpapier", Count: parseFloat(data.Toilettenpapier)},
-            {"Product": "Mehl", "Count": parseFloat(data.Mehl)},
-            {"Product": "Desinfektionsmittel", Count: parseFloat(data.Desinfektionsmittel)},
-            {"Product": "Hefe", "Count": parseFloat(data.Hefe)}
-        ]
-
-        var text = svg.selectAll(".hunder")
-            .remove();
-
-        var images = svg.selectAll("image")
-            .remove();
-
-        var bars = svg.selectAll(".bar")
-            .data(newData);
-
-        bars.exit()
-            .attr("hight", 0)
-            .remove();
-
-        bars.enter()//this is the enter selection
-            .append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) { return d.Product; })
-                .attr("y", function(d) {return d.Count;})
-                .attr("height", function(d) {10})
-                .attr("width", 21.57894736842106)
-                .attr("fill", "#9de0ff")
-                .merge(bars)//and from now on, both the enter and the update selections
-                //.transition()
-                //.duration(1000)
-                //.delay(1000)
-                //.attr("y", function(d) { return d.Count; })
-                //.attr("height", function(d) { height - d.Count})
-
-        //Bilder
-        var imgSeife = svg.append('image')
-        .attr('href', './Pictures/Verkaufszahlen/Seife_oB.png')
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr("x", 36 + 54)
-        .attr("y",  110 + newData[0].Count );
-        
-        var imgToi = svg.append('image')
-        .attr('href', './Pictures/Verkaufszahlen/Klopapier.png')
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr("x", 108 + 54)
-        .attr("y", 105 + newData[1].Count );
-        
-        var imgMehl = svg.append('image')
-        .attr('href', './Pictures/Verkaufszahlen/Mehl.png')
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr("x", 180 + 54)
-        .attr("y", 100 + newData[2].Count );
-        
-        var imgDes = svg.append('image')
-        .attr('href', './Pictures/Verkaufszahlen/Desinfektionsmittel.png')
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr("x", 252 + 54)
-        .attr("y", 105 + newData[3].Count );
-        
-        var imgHefe = svg.append('image')
-        .attr('href', './Pictures/Verkaufszahlen/Hefe.png')
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr("x", 324 + 54)
-        .attr("y",  90 + newData[4].Count );
-        
-        
-        //Zahlen über Balken
-        var textSeife = svg.append("text")  
-            .attr("class", "hunder")
-            .text(newData[0].Count.toString()) 
-            .attr("transform", "translate("+ (61-newData[0].Count.toString().length*3 + 54 ) + "," + (70 + newData[0].Count + 54 ) + ")")
-            .attr("font-family", "arial")
-            .attr("font-size","11px")
-            .style("fill", "white")
-            .style("text-align", "center");
-        
-        var textToi = svg.append("text")  
-            .attr("class", "hunder")
-            .text(newData[1].Count.toString())
-            .attr("transform", "translate("+ (132-newData[1].Count.toString().length*3 + 54) + "," + (95 + newData[1].Count) + ")")
-            .attr("font-family", "arial")
-            .attr("font-size","11px")
-            .style("fill", "white");
-        
-        var textMehl = svg.append("text")  
-            .attr("class", "hunder")
-            .text(newData[2].Count.toString())
-            .attr("transform", "translate("+ (205-newData[2].Count.toString().length*3 + 54) + "," + (95 + newData[2].Count) + ")")
-            .attr("font-family", "arial")
-            .attr("font-size","11px")
-            .style("fill", "white");
-        
-        var textDes = svg.append("text")  
-            .attr("class", "hunder")
-            .text(newData[3].Count.toString())
-            .attr("transform", "translate("+ (277-newData[3].Count.toString().length*3 + 54) + "," + (103 + newData[3].Count) + ")")
-            .attr("font-family", "arial")
-            .attr("font-size","11px")
-            .style("fill", "white");
-        
-        var textHefe = svg.append("text")  
-            .attr("class", "hunder")
-            .text(newData[4].Count.toString())
-            .attr("transform", "translate("+ (348-newData[4].Count.toString().length*3 + 54) + "," + (68 + newData[4].Count + 24) + ")")
-            .attr("font-family", "arial")
-            .attr("font-size","11px")
-            .style("fill", "white");
-        }
 
 });
 }
